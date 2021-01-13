dafc74c73aa3d3b00badfed3f328a53ff979a8ae
scalasbtprojects <scalasbtprojects@gmail.com>
2021-01-13 20:37:21 +0100

    Avoid shutting down bot on chat m

--- src/lichessbot.rs
+++ src/lichessbot.rs
@@ -97,74 +97,72 @@ impl LichessBot {
 				}
 			};
 
-			if state_opt.is_none() {
-				return Ok(())
-			}
+			if state_opt.is_some() {
+				let state = state_opt.unwrap();
 
-			let state = state_opt.unwrap();
+				println!("state {:?}", state);
 
-			let fen = make_uci_moves(state.moves.as_str())?;
+				let fen = make_uci_moves(state.moves.as_str())?;
 
-			println!("fen {}", fen);
+				println!("fen {}", fen);
 
-			let setup: Fen = fen.parse()?;
-			let pos: Chess = setup.position(shakmaty::CastlingMode::Standard)?;
+				let setup: Fen = fen.parse()?;
+				let pos: Chess = setup.position(shakmaty::CastlingMode::Standard)?;
 
-			let legals = pos.legals();
+				let legals = pos.legals();
 
-			let rand_move = legals.choose(&mut rand::thread_rng()).unwrap();
+				let rand_move = legals.choose(&mut rand::thread_rng()).unwrap();
 
-			let rand_uci = Uci::from_standard(&rand_move).to_string();
+				let rand_uci = Uci::from_standard(&rand_move).to_string();
 
-			println!("rand uci {}", rand_uci);
+				println!("rand uci {}", rand_uci);
 
-			let turn = setup.turn;
+				let turn = setup.turn;
 
-			println!("turn {:?}", turn);
+				println!("turn {:?}", turn);
 
-			let bot_turn = ( ( turn == Color::White ) && bot_white ) || ( ( turn == Color::Black ) && !bot_white );
+				let bot_turn = ( ( turn == Color::White ) && bot_white ) || ( ( turn == Color::Black ) && !bot_white );
 
-			println!("bot turn {}", bot_turn);
+				println!("bot turn {}", bot_turn);
 
-			if bot_turn {
-				let id = game_id.to_owned();
+				if bot_turn {
+					let id = game_id.to_owned();
 
-				let moves = format!("{}", state.moves);
+					let moves = format!("{}", state.moves);
 
-				let go_job = GoJob::new()
-					.uci_opt("UCI_Variant", "chess")
-					.pos_startpos()
-					.pos_moves(moves)
-					.tc(Timecontrol{
-						wtime: state.wtime as usize,
-						winc: state.winc as usize,
-						btime: state.btime as usize,
-						binc: state.binc as usize
-					})
-				;
+					let go_job = GoJob::new()
+						.uci_opt("UCI_Variant", "chess")
+						.pos_startpos()
+						.pos_moves(moves)
+						.tc(Timecontrol{
+							wtime: state.wtime as usize,
+							winc: state.winc as usize,
+							btime: state.btime as usize,
+							binc: state.binc as usize
+						})
+					;
 
-				println!("engine start thinking on {:?}", go_job);
+					println!("engine start thinking on {:?}", go_job);
 
-				let go_result = engine.go(go_job).recv().await;
+					let go_result = engine.go(go_job).recv().await;
 
-				println!("thinking result {:?}", go_result);
+					println!("thinking result {:?}", go_result);
 
-				let mut bestmove = rand_uci;
+					let mut bestmove = rand_uci;
 
-				if let Some(go_result) = go_result {
-					if let Some(bm) = go_result.bestmove {
-						bestmove = bm;
+					if let Some(go_result) = go_result {
+						if let Some(bm) = go_result.bestmove {
+							bestmove = bm;
+						}
 					}
+
+					println!("making move {}", bestmove);
+
+					let result = self.lichess.make_a_bot_move(id.as_str(), bestmove.as_str(), false).await;
+
+					println!("make move result {:?}", result);
 				}
-
-				println!("making move {}", bestmove);
-
-				let result = self.lichess.make_a_bot_move(id.as_str(), bestmove.as_str(), false).await;
-
-				println!("make move result {:?}", result);
 			}
-
-			println!("state {:?}", state);
 		}
 
 		Ok(())
