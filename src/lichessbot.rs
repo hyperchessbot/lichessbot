6fd0859f4bd703b7d95ea0d5b57932af1b75b42f
scalasbtprojects <scalasbtprojects@gmail.com>
2021-01-13 18:33:28 +0100

    Determine players

--- src/lichessbot.rs
+++ src/lichessbot.rs
@@ -1,6 +1,7 @@
 use futures_util::TryStreamExt;
 use licoricedev::client::{Lichess};
-use licoricedev::models::board::{Event};
+use licoricedev::models::board::{Event, BoardState};
+use licoricedev::models::board::Challengee::{LightUser, StockFish};
 
 /// lichess bot
 pub struct LichessBot {
@@ -16,6 +17,68 @@ impl LichessBot {
 		}
 	}
 
+	/// play game
+	async fn play_game(&mut self, game_id: String) -> Result<(), Box::<dyn std::error::Error>> {
+		println!("playing game {}", game_id);
+
+		let mut game_stream = self.lichess
+			.stream_bot_game_state(&game_id)
+			.await
+			.unwrap();
+		
+		let mut bot_white = true;					
+		
+		while let Some(game_event) = game_stream.try_next().await? {
+			println!("{:?}", game_event);
+
+			let white:String;
+			let black:String;
+			let bot = std::env::var("RUST_BOT_NAME").unwrap();				
+			
+			let state_opt = match game_event {
+				BoardState::GameFull ( game_full ) => {
+					println!("game full {:?}", game_full);
+					
+					white = match game_full.white {
+						LightUser(user) => user.username,
+						StockFish(sf) => format!("Stockfish AI level {}", sf.ai_level)
+					};
+
+					black = match game_full.black {
+						LightUser(user) => user.username,
+						StockFish(sf) => format!("Stockfish AI level {}", sf.ai_level)
+					};
+					
+					if bot == black {
+						bot_white = false;
+					}
+					
+					println!("**************\n{} - {} bot white {}\n**************", white, black, bot_white);
+					
+					Some(game_full.state)
+				},
+				BoardState::GameState ( game_state ) => {
+					Some(game_state)
+				},
+				_ => {
+					println!("undhandled game event {:?}", game_event);
+
+					None
+				}
+			};
+
+			if state_opt.is_none() {
+				return Ok(())
+			}
+
+			let state = state_opt.unwrap();
+
+			println!("state {:?}", state);
+		}
+
+		Ok(())
+	}
+
 	/// process event stream event
 	async fn process_event_stream_event(&mut self, event: Event) -> Result<(), Box::<dyn std::error::Error>> {
 		println!("event {:?}", event);
@@ -29,11 +92,18 @@ impl LichessBot {
 						println!("rejecting challenge, correspondence");
 					} else {
 						println!("accepting challenge, response {:?}",
-							self.lichess.challenge_accept(&challenge.id).await.unwrap());															}
+							self.lichess.challenge_accept(&challenge.id).await);															}
 				} else {
 					println!("rejecting challenge, wrong variant {}", challenge.variant.key);
 				}
 			},
+			Event::GameStart { game } => {
+				let game_id = format!("{}", game.id);
+
+				println!("game started {}", game_id);
+
+				println!("play game result {:?}", self.play_game(game_id).await);
+			}
 			_ => {
 				println!("unhandled event")
 			}
